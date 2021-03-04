3bbf3ef452f95fcf35311110fa17728dcfc34ec1
Nishant0468 <78916026+Nishant0468@users.noreply.github.com>
2021-03-04 20:07:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,27 @@
-var a ,b;
+var fixedRect, movingRect;
+var a,b
+
 function setup() {
-  createCanvas(800,400);
-  a = createSprite(400, 200, 50, 50);
-  b = createSprite(200,200,30,70);
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  a = createSprite(300,100,40,50);
+  a.shapeColor = "red";
+  b = createSprite(300,300,40,50);
+  b.shapeColor = "red";
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
+  a.velocityY = +5;
+  b.velocityY = -5;
 }
 
 function draw() {
-  background(255,255,255);  
-  a.x = World.mouseX;
-  a.y = World.mouseY;
-  console.log  ( a.x-b.x);
-  if (a.x-b.x  < a.width/2 + b.width/2 && b.x-a.x  < a.width/2 + b.width/2 && a.y-b.y < a.height/2 + b.height/2 && b.y-a.y < a.height/2 + b.height/2){
-    a.shapeColor = "red";
-    b.shapeColor = "red";
-    
-  }
-  else{
-    a.shapeColor = "green";
-    b.shapeColor = "green";
-  }
+  background(0,0,0);  
+
+  bounceOff(a,b);
 
   drawSprites();
 }
